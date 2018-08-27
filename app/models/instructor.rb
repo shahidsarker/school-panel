26f06668395bd462c0df9e43d186444852a1a98b
Shahid <shahid.sarker@gmail.com>
2018-08-27 17:02:24 -0400

    ya

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,6 +1,9 @@
 class Instructor < ApplicationRecord
   has_many :cohorts
   has_many :courses, through: :cohorts
+  # enum education: [' ','High School', "Bachelor's", "Master's", "PhD"]
+
+  enum education: {'High School':1, "Bachelor's":2, "Master's":3, "PhD":4}
 
   validates :first_name, :last_name, presence: true
   validates :age, numericality: {only_integer: true,greater_than: 0,less_than: 150}
@@ -11,8 +14,8 @@ class Instructor < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
-  def edu_level(input=self.education)
-    education_levels = ['High School', "Bachelor's", "Master's", "PhD"]
-    education_levels[input-1]
-  end
+  # def edu_level(input=self.education)
+  #   education_levels = ['High School', "Bachelor's", "Master's", "PhD"]
+  #   education_levels[input-1]
+  # end
 end
